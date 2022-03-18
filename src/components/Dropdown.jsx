b53d136eab1c211b756d311dad465b3b5f1494e5
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-19 00:06:16 +0300

    Add dropdown component to project

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
+import {faCaretDown} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const Dropdown = ({ selected, setSelected }) => {
+const Dropdown = ({selected, setSelected}) => {
     const [isActive, setIsActive] = useState(false);
     const options = ["Hairstyle", "Makeup"];
 
     return (
         <div className="dropdown">
-            <div className="dropdown-btn" onClick={ () => setIsActive(!isActive)}>
+            <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>
                 {selected}
-                <span className="fas fa-caret-down"/>
+                <span/>
+                <FontAwesomeIcon icon={faCaretDown}/>
             </div>
-            { isActive && (
+            {isActive && (
                 <div className="dropdown-content">
-                    { options.map(option => (
+                    {options.map(option => (
                         <div className="dropdown-item" onClick={() => {
                             setSelected(option)
                             setIsActive(false)
